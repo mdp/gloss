9d67dfc4c94ad267faabdba8e8a379724c011870
mdp <m@mdp.im>
2015-09-16 16:50:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# GLoSS (Go SSL)
+Develop on Localhost with SSL
+
+## Why
+
+Your webapp runs on HTTPS, but you develop locally on HTTP. That's not right.
+
+GLoSS is a very simple HTTPS reverse proxy, which sets up and serves a TLS/SSL
+frontend to your apps.
+
+## How
+
+1. Pick a hostname for your local development
+  a. `echo "127.0.0.1   local.dev" | sudo tee /etc/hosts > /dev/null`
+  a. `echo "127.0.0.1   foo.local.dev" | sudo tee /etc/hosts > /dev/null`
+1. Setup the certificate
+  a. `gossl setup --host "local.dev,*.local.dev"`
+1. Import the certificate to your keychain (Mac specific instructions below)
+  - `open ~/.gloss/cert.pem`
+  - Find the GLoSS cert and make it "Trusted"
+1. Start using GLoSS
+  - `gloss --map "*:3000,foo:4000"` Maps foo.local.dev to port 4000, everything else to 3000
+1. Visit https://foo.local.dev:4443
+  - Will return the content at localhost:4000 via HTTPS
+
+## Notes
+
+- Passes the same headers you'd expect with reverse proxy ssl
+  - "X-Forwarded-Proto": "https"
+  - "X-Forwarded-For": "the.clients.real.ip"
+- Doesn't require trusting a CA cert, only valid signing for the \*.local.dev hostname for example.
+- Works on a variety of architectures with zero dependencies thanks to Golang (ARM, x86)
+
+## License: MIT
+
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	mappings   = flag.String("map", "", "Mappings of hostnames to ports")
+	path       = flag.String("cert_path", os.Getenv("HOME")+"/.gloss", "Path to certification location")
+	port       = flag.Int("port", 4443, "TLS/SSL port to listen on")
+	host       = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
+	validFrom  = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
+	validFor   = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	isCA       = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
+	rsaBits    = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+	ecdsaCurve = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256, P384, P521")
+)
+
+var hostPortMapping = make(map[string]int)
+
+func multipleHostReverseProxy(hostMapping *map[string]int) *httputil.ReverseProxy {
+	// Fairly simple right now:
+	// use the subdomain to route to a specific port
+	director := func(req *http.Request) {
+		domains := strings.Split(req.Host, ".")
+		fmt.Printf("%v", req.Host)
+		topSubdomain := domains[0]
+		port := hostPortMapping["*"]
+		if hostPortMapping[topSubdomain] > 0 {
+			port = hostPortMapping[topSubdomain]
+			fmt.Printf("%v", port)
+		}
+		req.URL.Scheme = "http"
+		req.URL.Host = "localhost:" + strconv.Itoa(port)
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func generateCertificate() {
+	cert := Certificate{
+		host:       host,
+		path:       path,
+		validFrom:  validFrom,
+		validFor:   validFor,
+		isCA:       isCA,
+		rsaBits:    rsaBits,
+		ecdsaCurve: ecdsaCurve,
+	}
+
+	cert.Generate()
+}
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "setup" {
+		flag.CommandLine.Parse(os.Args[2:])
+		fmt.Println("Generating certificates")
+		generateCertificate()
+		return
+	}
+	flag.CommandLine.Parse(os.Args[1:])
+	if len(*mappings) < 1 {
+		log.Fatal("You must specify the host mapping. Eg: --map 'foo:3000,*:4000'")
+		return
+	}
+	for _, mapping := range strings.Split(*mappings, ",") {
+		mapping = strings.TrimSpace(mapping)
+		s := strings.Split(mapping, ":")
+		proxyPort, err := strconv.Atoi(s[1])
+		if err != nil {
+			log.Fatalf("Mapping error: %s", err)
+		}
+		if s[0] == "*" {
+			fmt.Printf("Mapping * to %d\n", proxyPort)
+		} else {
+			fmt.Printf("Mapping %s.* to %d\n", s[0], proxyPort)
+		}
+		hostPortMapping[s[0]] = proxyPort
+	}
+	cert, err := GetCerts(path)
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	config.Rand = rand.Reader
+	service := "0.0.0.0:" + strconv.Itoa(*port)
+	fmt.Printf("Listening for SSL on %s\n", service)
+	listener, err := tls.Listen("tcp", service, &config)
+	proxy := multipleHostReverseProxy(&hostPortMapping)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("X-Forwarded-Proto", "https")
+		w.Header().Add("X-Forwarded-For", r.RemoteAddr)
+		proxy.ServeHTTP(w, r)
+	})
+	log.Fatal(http.Serve(listener, nil))
+}
